6dfb4cf0e38cefc4302756c4e20568a2b2fd408f
Tankcat <tankcat_2@163.com>
2017-11-28 09:35:55 +0800

    Site updated: 2017-11-28 09:35:50

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-!function(t){var i=t("#sidebar").height()-t(window).height()+60;if(t(".article-entry").each(function(i){t(this).find("img").each(function(){this.alt&&t(this).after('<span class="caption">'+this.alt+"</span>"),t(this).wrap('<a href="'+this.src+'" title="'+this.alt+'" class="gallery-item"></a>')})}),lightGallery&&(l={selector:".gallery-item"},lightGallery(t(".article-entry")[0],l),lightGallery(t(".article-gallery")[0],l)),t.prototype.justifiedGallery){var l={rowHeight:140,margins:4,lastRow:"justify"};t(".justified-gallery").justifiedGallery(l)}t("#sidebar").length&&t(document).on("scroll",function(){t(document).width()>=800?t(this).scrollTop()>i&&t(this).scrollTop()>0?(t("#toTop").fadeIn(),t("#toTop").css("left",t("#sidebar").offset().left)):t("#toTop").fadeOut():(t("#toTop").fadeIn(),t("#toTop").css("right",20))}).on("click","#toTop",function(){t("body, html").animate({scrollTop:0},600)})}(jQuery);
+(function($){
+    var toTop = ($('#sidebar').height() - $(window).height()) + 60;
+    // Caption
+    $('.article-entry').each(function(i) {
+        $(this).find('img').each(function() {
+            if (this.alt) {
+                $(this).after('<span class="caption">' + this.alt + '</span>');
+            }
+
+            $(this).wrap('<a href="' + this.src + '" title="' + this.alt + '" class="gallery-item"></a>');
+        });
+
+    });
+    if (lightGallery) {
+        var options = {
+            selector: '.gallery-item',
+        };
+        lightGallery($('.article-entry')[0], options);
+        lightGallery($('.article-gallery')[0], options);
+    }
+    if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
+        var options = {
+            rowHeight: 140,
+            margins: 4,
+            lastRow: 'justify'
+        };
+        $('.justified-gallery').justifiedGallery(options);
+    }
+
+    // Profile card
+    
+
+    // To Top
+    if ($('#sidebar').length) {
+        $(document).on('scroll', function () {
+            if ($(document).width() >= 800) {
+                if(($(this).scrollTop() > toTop) && ($(this).scrollTop() > 0)) {
+                    $('#toTop').fadeIn();
+                    $('#toTop').css('left', $('#sidebar').offset().left);
+                } else {
+                    $('#toTop').fadeOut();
+                }
+            } else {
+                $('#toTop').fadeIn();
+                $('#toTop').css('right', 20);
+            }
+        }).on('click', '#toTop', function () {
+            $('body, html').animate({ scrollTop: 0 }, 600);
+        });
+    }
+
+})(jQuery);
--- vendor/lightgallery/js/lg-hash.js
+++ vendor/lightgallery/js/lg-hash.js
@@ -1,1 +1,97 @@
-!function(t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):("undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:this).LgHash=t()}(function(){return function t(o,e,i){function n(s,l){if(!e[s]){if(!o[s]){var h="function"==typeof require&&require;if(!l&&h)return h(s,!0);if(r)return r(s,!0);var a=new Error("Cannot find module '"+s+"'");throw a.code="MODULE_NOT_FOUND",a}var d=e[s]={exports:{}};o[s][0].call(d.exports,function(t){return n(o[s][1][t]||t)},d,d.exports,t,o,e,i)}return e[s].exports}for(var r="function"==typeof require&&require,s=0;s<i.length;s++)n(i[s]);return n}({1:[function(t,o,e){!function(t,o){void 0!==e?o():(o(),t.lgHash={})}(this,function(){"use strict";var t=Object.assign||function(t){for(var o=1;o<arguments.length;o++){var e=arguments[o];for(var i in e)Object.prototype.hasOwnProperty.call(e,i)&&(t[i]=e[i])}return t},o={hash:!0},e=function(e){return this.el=e,this.core=window.lgData[this.el.getAttribute("lg-uid")],this.core.s=t({},o,this.core.s),this.core.s.hash&&(this.oldHash=window.location.hash,this.init()),this};e.prototype.init=function(){var t,o=this;utils.on(o.core.el,"onAfterSlide.lgtm",function(t){window.location.hash="lg="+o.core.s.galleryId+"&slide="+t.detail.index}),utils.on(window,"hashchange.lghash",function(){t=window.location.hash;var e=parseInt(t.split("&slide=")[1],10);t.indexOf("lg="+o.core.s.galleryId)>-1?o.core.slide(e,!1,!1):o.core.lGalleryOn&&o.core.destroy()})},e.prototype.destroy=function(){this.core.s.hash&&(this.oldHash&&this.oldHash.indexOf("lg="+this.core.s.galleryId)<0?window.location.hash=this.oldHash:history.pushState?history.pushState("",document.title,window.location.pathname+window.location.search):window.location.hash="",utils.off(this.core.el,".lghash"))},window.lgModules.hash=e})},{}]},{},[1])(1)});
+/**!
+ * lg-hash.js | 0.0.1 | August 1st 2016
+ * http://sachinchoolur.github.io/lg-hash.js
+ * Copyright (c) 2016 Sachin N; 
+ * @license Apache 2.0 
+ */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.LgHash = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof exports !== "undefined") {
+        factory();
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory();
+        global.lgHash = mod.exports;
+    }
+})(this, function () {
+    'use strict';
+
+    var _extends = Object.assign || function (target) {
+        for (var i = 1; i < arguments.length; i++) {
+            var source = arguments[i];
+
+            for (var key in source) {
+                if (Object.prototype.hasOwnProperty.call(source, key)) {
+                    target[key] = source[key];
+                }
+            }
+        }
+
+        return target;
+    };
+
+    var hashDefaults = {
+        hash: true
+    };
+    var Hash = function Hash(element) {
+        this.el = element;
+        this.core = window.lgData[this.el.getAttribute('lg-uid')];
+        this.core.s = _extends({}, hashDefaults, this.core.s);
+        if (this.core.s.hash) {
+            this.oldHash = window.location.hash;
+            this.init();
+        }
+
+        return this;
+    };
+
+    Hash.prototype.init = function () {
+        var _this = this;
+        var _hash;
+
+        // Change hash value on after each slide transition
+        utils.on(_this.core.el, 'onAfterSlide.lgtm', function (event) {
+            window.location.hash = 'lg=' + _this.core.s.galleryId + '&slide=' + event.detail.index;
+        });
+
+        // Listen hash change and change the slide according to slide value
+        utils.on(window, 'hashchange.lghash', function () {
+            _hash = window.location.hash;
+            var _idx = parseInt(_hash.split('&slide=')[1], 10);
+
+            // it galleryId doesn't exist in the url close the gallery
+            if (_hash.indexOf('lg=' + _this.core.s.galleryId) > -1) {
+                _this.core.slide(_idx, false, false);
+            } else if (_this.core.lGalleryOn) {
+                _this.core.destroy();
+            }
+        });
+    };
+
+    Hash.prototype.destroy = function () {
+        if (!this.core.s.hash) {
+            return;
+        }
+
+        // Reset to old hash value
+        if (this.oldHash && this.oldHash.indexOf('lg=' + this.core.s.galleryId) < 0) {
+            window.location.hash = this.oldHash;
+        } else {
+            if (history.pushState) {
+                history.pushState('', document.title, window.location.pathname + window.location.search);
+            } else {
+                window.location.hash = '';
+            }
+        }
+
+        utils.off(this.core.el, '.lghash');
+    };
+
+    window.lgModules.hash = Hash;
+});
+
+},{}]},{},[1])(1)
+});
