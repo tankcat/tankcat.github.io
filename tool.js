57467c6a1412a9959f472c2e890810991d5dfebe
Tankcat <tankcat_2@163.com>
2017-12-12 10:36:29 +0800

    Site updated: 2017-12-12 10:36:28

--- tool.js
+++ tool.js
@@ -1,1 +1,25 @@
-"use strict";var fs=require("fs"),path="photos/";fs.readdir(path,function(t,s){if(!t){var i=[];!function t(e){if(e==s.length)return fs.writeFile("photos/data.json",JSON.stringify(i,null,"\t")),void console.log("get img success!");fs.stat(path+s[e],function(n,o){n||(o.isFile()&&i.push(s[e]),t(e+1))})}(0)}});
+"use strict";
+var fs = require("fs");
+var path = "photos/";
+fs.readdir(path, function (err, files) {
+    if (err) {
+        return;
+    }
+    var arr = [];
+    (function iterator(index) {
+        if (index == files.length) {
+            fs.writeFile("photos/data.json", JSON.stringify(arr, null, "\t"));
+            console.log('get img success!');
+            return;
+        }
+        fs.stat(path + files[index], function (err, stats) {
+            if (err) {
+                return;
+            }
+            if (stats.isFile()) {
+                arr.push(files[index]);
+            }
+            iterator(index + 1);
+        })
+    }(0));
+});
